0f615928e335b2341781d6eb6e1ddfbc4a292693
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-29 11:19:35 +0330

    create data class

---
+++ AIClass/AIClass/AIClass/AI/BlindSearch/Datacs.cs
@@ -0,0 +1,16 @@
+﻿using DataStructures.Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIClass.AI.BlindSearch
+{
+    internal class Data<V>
+    {
+        public IVertex<V> vertex;
+        public bool VF;
+        public bool VNF;
+    }
+}
